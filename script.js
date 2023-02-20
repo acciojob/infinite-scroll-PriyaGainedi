4d3eaf28dd6713697831c4349f95e23860eb9404
Monika Priya Gainedi <priyagainedi8@gmail.com>
2023-02-20 22:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //your code here!
+var list = document.getElementById("infi-list");
+for(var i=1;i<=10;i++){
+	var listItem = document.createElement("li");
+	listItem.textContent ="item "+i;
+	list.appendChild(listItem);
+}
+function addlistItems() {
+	for(var i=11;i<=50;i++){
+		var listItem = document.createElement("li");
+		listItem.textContent ="item "+i;
+		list.appendChild(listItem);
+	}
+}
 
+list.addEventListener("scroll",addlistItems);
